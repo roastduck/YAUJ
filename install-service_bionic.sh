698358daacb790a10143258218166a56aa6544bf
Michael Long <mchobbylong@gmail.com>
2019-06-09 01:02:45 +0800

    fix: fix typo

--- install-service_bionic.sh
+++ install-service_bionic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp yaujd.example /etc/init.d/
+sudo cp yaujd.example /etc/init.d/yaujd
 sudo chmod 755 /etc/init.d/yaujd
 sudo update-rc.d yaujd defaults
 echo "Installed service 'yaujd' (run as root)!"
